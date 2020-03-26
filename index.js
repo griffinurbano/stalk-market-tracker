6fb3e9bdf24de0a9a1ba0978353b37a63c6c9a41
Glitch (stalk-market-tracker) <none>
2020-03-26 05:06:24 +0000

    👧👚 Updated with Glitch

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require('discord.js');
 const Sequelize = require('sequelize');
 
@@ -154,4 +166,4 @@ client.on('message', async message => {
 	}
 });
 
-client.login('NjkxODY0MTg3MDA5NDk5MTg2.XnmK1w.qbbyMLQ_GyikTheEfyphBJSdSxY');
+client.login(process.env.TOKEN);
